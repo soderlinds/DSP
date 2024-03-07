58d389d98d4f23851893d8db97d406f93d4cff5f
soderlinds <112867382+soderlinds@users.noreply.github.com>
2024-03-07 16:21:20 +0100

    settings for deployment updated

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -1,17 +1,27 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const mnemonic = process.env.MNEMONIC;
+console.log("Mnemonic:", mnemonic);
+
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545, 
-      network_id: "*",
+    goerli: {
+      provider: () => new HDWalletProvider({
+        mnemonic: mnemonic,
+        providerOrUrl: `https://goerli.infura.io/v3/7c2ba60ad62447a6a8dac2b8cd73bb4a`
+      }),
+      network_id: 5,
+      gas: 5500000, 
+      confirmations: 2, 
+      timeoutBlocks: 200, 
+      skipDryRun: true, 
     },
   },
   compilers: {
     solc: {
-      version: "0.8.0",
-    },
-  },
-  mocha: {
-    // timeout: 100000
-  },
+      version: "^0.8.0", 
+    }
+  }
 };
