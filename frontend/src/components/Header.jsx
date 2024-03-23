670d577f0d6d2b36142ca06f81fa372069688e1c
soderlinds <112867382+soderlinds@users.noreply.github.com>
2024-03-23 14:04:00 +0100

    farcaster test

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,7 +12,8 @@ const Header = () => {
   const contractCreatorAddress = '0x2dCb11EeD42F6199658B66BC45D24470CcE2B710'; 
   const isAdmin = user && user.wallet && user.wallet.address.toLowerCase() === contractCreatorAddress.toLowerCase();
   const isLoggedIn = user;
-
+  const hasFarcasterAccount = user && user.farcaster;
+  
   return (
     <header className="header">
       <nav className="nav">
@@ -24,7 +25,7 @@ const Header = () => {
             <li><NavLink to="/market" activeClassName="active">MARKET</NavLink></li>
             <li><NavLink to="/rewards" activeClassName="active">NFTs</NavLink></li>
             <li><NavLink to="/leaderboard" activeClassName="active">LEADERBOARD</NavLink></li>
-            <li><NavLink to="/farcaster" activeClassName="active">FARCASTER</NavLink></li>
+            {hasFarcasterAccount && <li><NavLink to="/farcaster" activeClassName="active">FARCASTER</NavLink></li>} 
             {isLoggedIn && <li><NavLink to="/mywallet" activeClassName="active">MY COLLECTION</NavLink></li>}
             {isAdmin && <li><NavLink to="/admin" activeClassName="active">ADMIN</NavLink></li>}
           </ul>
